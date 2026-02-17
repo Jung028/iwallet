d93c12254c67fa37ce5bb8fe7b7757b5ad8670fe
Jung028 <aedamjung@gmail.com>
2026-02-17 00:12:37 -0800

    update connection and enums value of

--- app/core/model/src/main/java/com/alipay/business/core/model/converter/ModelConverter.java
+++ app/core/model/src/main/java/com/alipay/business/core/model/converter/ModelConverter.java
@@ -3,6 +3,7 @@ package com.alipay.business.core.model.converter;
 
 import com.alipay.alipay_plus.common.dal.auto.dataobject.IdempotencyKeysDO;
 import com.alipay.business.core.model.domain.IdempotencyKeys;
+import com.alipay.business.core.model.enums.IdempotencyKeysStatusEnum;
 
 /**
  * @author jung
@@ -19,7 +20,7 @@ public class ModelConverter {
         keys.setIdempotencyKey(idempotencyKeysDO.getIdempotencyKey());
         keys.setRequestHash(idempotencyKeysDO.getRequestHash());
         keys.setTxnId(idempotencyKeysDO.getTxnId());
-        keys.setStatus(idempotencyKeysDO.getStatus());
+        keys.setStatus(IdempotencyKeysStatusEnum.valueOf(idempotencyKeysDO.getStatus()));
         keys.setErrorCode(idempotencyKeysDO.getErrorCode());
         keys.setResponseSnapshot(idempotencyKeysDO.getResponseSnapshot());
         keys.setCreatedAt(idempotencyKeysDO.getCreatedAt());
