2e6e5c77891731b7219a08fa33db10030ac45d26
Jung028 <aedamjung@gmail.com>
2026-03-06 22:24:09 +1100

    updated version for account and user

--- app/common/service/facade/src/main/java/com/alipay/business/common/service/facade/result/UpdateIdempotencyKeysResult.java
+++ app/common/service/facade/src/main/java/com/alipay/business/common/service/facade/result/UpdateIdempotencyKeysResult.java
@@ -5,6 +5,7 @@ import com.alipay.business.common.service.facade.baseresult.BusinessBaseResult;
 public class UpdateIdempotencyKeysResult extends BusinessBaseResult {
     private String status;
     private String txnId;
+    private int retryCount;
 
     public String getStatus() {
         return status;
@@ -20,5 +21,13 @@ public class UpdateIdempotencyKeysResult extends BusinessBaseResult {
 
     public void setTxnId(String txnId) {
         this.txnId = txnId;
+    }
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
     }
 }
--- app/web/src/main/java/com/alipay/business/web/BusinessBasicController.java
+++ app/web/src/main/java/com/alipay/business/web/BusinessBasicController.java
@@ -2,10 +2,15 @@ package com.alipay.business.web;
 
 import com.alipay.business.common.service.facade.api.BusinessService;
 import com.alipay.business.common.service.facade.baseresult.BusinessBizResult;
-import com.alipay.business.common.service.facade.request.TransferConfirmRequest;
-import com.alipay.business.common.service.facade.request.TransferRequest;
+import com.alipay.business.common.service.facade.item.IdempotencyKeysItem;
+import com.alipay.business.common.service.facade.request.*;
+import com.alipay.business.common.service.facade.result.BusinessBalanceResult;
+import com.alipay.business.common.service.facade.result.BusinessTransactionDetailsResult;
+import com.alipay.business.common.service.facade.result.BusinessTransactionHistoryResult;
+import com.alipay.business.common.service.facade.result.UpdateIdempotencyKeysResult;
 import com.alipay.sofa.runtime.api.annotation.SofaReference;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
@@ -17,7 +22,7 @@ public class BusinessBasicController {
     private BusinessService businessService;
 
     @PostMapping("/transfer.json")
-    public BusinessBizResult<String> transferInit(
+    public BusinessBizResult<String> transfer(
             @RequestBody TransferRequest request,
             HttpServletRequest httpServletRequest) {
         // retrieve JWT from header, verify JWT via public key.
@@ -29,7 +34,7 @@ public class BusinessBasicController {
         return businessService.transfer(request, userId);
     }
 
-    @PostMapping("/transfer/confirm.json")
+    @PostMapping("/transfer/transferConfirm.json")
     public BusinessBizResult<String> transferConfirm(
             @RequestBody TransferConfirmRequest request,
             HttpServletRequest httpServletRequest) {
@@ -42,6 +47,70 @@ public class BusinessBasicController {
         return businessService.transferConfirm(request, userId);
     }
 
+    @PostMapping("/transfer/queryTransactionDetails.json")
+    public BusinessBizResult<BusinessTransactionDetailsResult> queryTransactionDetails(
+            @RequestBody BusinessTransactionRecordRequest request,
+            HttpServletRequest httpServletRequest) {
+        // retrieve JWT from header, verify JWT via public key.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // get user id from auth principal
+        String userId = auth.getPrincipal().toString();
+
+        return businessService.queryTransactionDetails(request);
+    }
+
+    @PostMapping("/transfer/queryTransactionHistory.json")
+    public BusinessBizResult<BusinessTransactionHistoryResult> queryTransactionHistory(
+            @RequestBody BusinessTransactionHistoryRequest request,
+            HttpServletRequest httpServletRequest) {
+        // retrieve JWT from header, verify JWT via public key.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // get user id from auth principal
+        String userId = auth.getPrincipal().toString();
+
+        return businessService.queryTransactionHistory(request);
+    }
+
+    @PostMapping("/transfer/queryBalance.json")
+    public BusinessBizResult<BusinessBalanceResult> queryBalance(
+            @RequestBody BusinessBalanceRequest request,
+            HttpServletRequest httpServletRequest) {
+        // retrieve JWT from header, verify JWT via public key.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // get user id from auth principal
+        String userId = auth.getPrincipal().toString();
+
+        return businessService.queryBalance(request);
+    }
+
+    @PostMapping("/transfer/updateIdempotencyKeys.json")
+    public BusinessBizResult<UpdateIdempotencyKeysResult> updateIdempotencyKeys(
+            @RequestBody UpdateIdempotencyKeysRequest request,
+            HttpServletRequest httpServletRequest) {
+        // retrieve JWT from header, verify JWT via public key.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // get user id from auth principal
+        String userId = auth.getPrincipal().toString();
+
+        return businessService.updateIdempotencyKeys(request);
+    }
+
+    @PostMapping("/transfer/queryIdempotencyKeys.json")
+    public BusinessBizResult<IdempotencyKeysItem> queryIdempotencyKeys(
+            @RequestBody QueryIdempotencyKeysRequest request,
+            HttpServletRequest httpServletRequest) {
+        // retrieve JWT from header, verify JWT via public key.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // get user id from auth principal
+        String userId = auth.getPrincipal().toString();
+
+        return businessService.queryIdempotencyKeys(request);
+    }
 
     public void setBusinessService(BusinessService businessService) {
         this.businessService = businessService;
